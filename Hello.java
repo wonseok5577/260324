2b5ec65905e6644b2af2b06d8bd53a2272b1462c
wonseok5577 <wonseok5577@gmail.com>
2026-03-25 18:16:12 +0900

    주석 내용 추가

--- Hello.java
+++ Hello.java
@@ -3,15 +3,16 @@ package kr.ac.bu;
 // Hello 클래스 선언
 public class Hello {
 
-	// print() 메소드
+	// print() 메서드
 	public void print(String name) {
 		System.out.println("Hello, " + name);
 	}
 
+	// 진입 메서드
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hello hello = new Hello(); // new 키워드를 사용해 객체 생성
-		hello.print("wonseok");
+		hello.print("Java!");
 	}
 
 }
